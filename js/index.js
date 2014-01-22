3844323de23ebd85aa987e279f5f0bc5a6ff6c7b
ShaneHenchin <shanehenchin@gmail.com>
2014-01-22 22:28:02 +0000

    updates

--- js/index.js
+++ js/index.js
@@ -51,23 +51,7 @@ var app = {
             document.getElementById("info").innerHTML = result.text;
                     //var isbnText = "0735619670"
             
-                   var jq = document.createElement('script');
-        jq.src = "https://www.googleapis.com/books/v1/volumes?q=isbn:9780735619678&callback=handleResponse"
-        document.getElementsByTagName('head')[0].appendChild(jq);
 
-      function handleResponse(response) {
-      for (var i = 0; i < response.items.length; i++) {
-        var item = response.items[i];
-        document.getElementById("img1").src += item.volumeInfo.imageLinks.thumbnail;
-        document.getElementById("img1").width += 200;
-        document.getElementById("img1").height += 100;
-        document.getElementById("content").innerHTML += "<br>Title: " + item.volumeInfo.title;
-        document.getElementById("content").innerHTML += "<br>Author: " + item.volumeInfo.authors;
-        document.getElementById("content").innerHTML += "<br>Pulisher: " + item.volumeInfo.publisher;
-        document.getElementById("content").innerHTML += "<br>Published Date: " + item.volumeInfo.publishedDate;
-        document.getElementById("content").innerHTML += "<br>Book Summary: " + item.searchInfo.textSnippet;
-     }
-    } 
  
             
 
@@ -82,10 +66,25 @@ var app = {
             console.log("Scanning failed: ", error); 
         } );
     },
-
-
-    
+   
 };
 
+        var jq = document.createElement('script');
+        jq.src = "https://www.googleapis.com/books/v1/volumes?q=isbn:9780735619678&callback=handleResponse"
+        document.getElementsByTagName('head')[0].appendChild(jq);
+
+        function handleResponse(response) {
+        for (var i = 0; i < response.items.length; i++) {
+        var item = response.items[i];
+        //document.getElementById("img1").src += item.volumeInfo.imageLinks.thumbnail;
+        //document.getElementById("img1").width += 200;
+        //document.getElementById("img1").height += 100;
+        document.getElementById("content").innerHTML += "<br>Title: " + item.volumeInfo.title;
+        document.getElementById("content").innerHTML += "<br>Author: " + item.volumeInfo.authors;
+        document.getElementById("content").innerHTML += "<br>Pulisher: " + item.volumeInfo.publisher;
+        document.getElementById("content").innerHTML += "<br>Published Date: " + item.volumeInfo.publishedDate;
+        document.getElementById("content").innerHTML += "<br>Book Summary: " + item.searchInfo.textSnippet;
+     }
+    } 
 
         
