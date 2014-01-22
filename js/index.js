dd1ab190f3bfb94629a749e03302c1154d49bb95
ShaneHenchin <shanehenchin@gmail.com>
2014-01-22 13:38:37 +0000

    updates

--- js/index.js
+++ js/index.js
@@ -50,11 +50,11 @@ var app = {
                 "cancelled: " + result.cancelled + "\n");
             document.getElementById("info").innerHTML = result.text;
                     //var isbnText = "0735619670"
-            document.write("<script src='https://www.googleapis.com/books/v1/volumes?q=isbn:0735619670&callback=handleResponse'><\/script>");
+            //document.write("<script src='https://www.googleapis.com/books/v1/volumes?q=isbn:0735619670&callback=handleResponse'><\/script>");
             
 
             
-            //console.log(result);
+            console.log(result);
 
         }, 
         
