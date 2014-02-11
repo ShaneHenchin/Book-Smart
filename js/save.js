a7c3019b57eeb602a0304234d9d0bbcdd83fde64
ShaneHenchin <shanehenchin@gmail.com>
2014-02-11 15:50:01 +0000

    Updates

--- js/save.js
+++ js/save.js
@@ -1,15 +1,18 @@
  $('#btnSave').click(function(){
  	alert('About to call AJAX');
-        $.ajax({
-            type: 'POST',
-            url: 'http://booksmart.webege.com/save.php',
-            dataType: "json",
-            data: book_details,
-            success: function (msg) {
-                alert("success");
-            },
-            error: function (request, status, error) {
-                alert(error);
-            }
-        });
+ 	alert(book_details.title);
+ 	alert('after');
+    $.ajax({
+        type: 'POST',
+        url: 'http://booksmart.webege.com/save.php',
+        dataType: "json",
+        data: book_details,
+        success: function (msg) {
+            alert("success");
+        },
+        error: function (request, status, error) {
+            alert('error');
+            alert(error);
+        }
+    });
  });
