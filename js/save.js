b9c41c9fa774447d392eb4fa94e22a8313625993
ShaneHenchin <shanehenchin@gmail.com>
2014-02-12 15:18:46 +0000

    Updates

--- js/save.js
+++ js/save.js
@@ -16,15 +16,17 @@
 
  $('#searchBox').keyup(function(){
 var textVal = $("#searchBox").val();
-//alert(textVal);
+var data = {searchQuery: textVal};
+alert(textVal);
 $.ajax({ url: 'http://henchinshoodies.comuv.com/search2.php',
          dataType: "json",
          data: {searchQuery: textVal},
          type: 'post',
          success: function(output) {
+            alert(success);
             alert(output);
                       $('#resultsDiv').html(output);
-                  },
+        },
         error: function (request, status, error) {
             alert('error');
             alert(error);
