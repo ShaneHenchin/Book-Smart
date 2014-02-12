d2258d55334d7e02858fb6a0571103b526f1776c
ShaneHenchin <shanehenchin@gmail.com>
2014-02-12 13:57:16 +0000

    Updates

--- js/save.js
+++ js/save.js
@@ -2,10 +2,11 @@
  	alert(book_details.title);
  	alert(book_details.authors);
  	alert(book_details.publisher);
+    var data = JSON.stringify(book_details);
  	$.ajax({ 
 		url: 'http://booksmart.webege.com/save.php',
          dataType: "json",
-         data: book_details,
+         data: data,
          type: 'post',
          success: function(output) {
             alert("success");
@@ -15,27 +16,15 @@
             alert(error);
         }
 	});
-    $.ajax({
-        type: 'POST',
-        url: 'http://booksmart.webege.com/save.php',
-        dataType: "json",
-        data: book_details,
-        success: function (msg) {
-            alert("success");
-        },
-        error: function (request, status, error) {
-            alert('error');
-            alert(error);
-        }
     });
- });
 
  $('#searchBox').keyup(function(){
 var textVal = $("#searchBox").val();
+var data = JSON.stringify({searchQuery: textVal});
 //alert(textVal);
 $.ajax({ url: 'http://booksmart.webege.com/search.php',
          dataType: "json",
-         data: {searchQuery: textVal},
+         data: data,
          type: 'post',
          success: function(output) {
             alert(output);
