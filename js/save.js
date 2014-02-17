0f64c910559b37e8c5418809a2d4e989af304721
ShaneHenchin <shanehenchin@gmail.com>
2014-02-17 16:56:57 +0000

    Updates

--- js/save.js
+++ js/save.js
@@ -9,32 +9,19 @@
     //var data = JSON.stringify(book_details);
 
     $.ajax({ 
-
         url: 'http://henchinshoodies.comuv.com/save.php',
-
          dataType: "json",
-
          data: book_details,
-
          type: 'post',
-
          success: function(output) {
-
             alert("success");
-
          },
-
         error: function (request, status, error) {
-
             alert('error');
-
             alert(error);
-
         }
-
     });
-
-    });
+});
 
  
 
