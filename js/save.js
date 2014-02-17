087841e36aaae8dd5544c3868f29a094f4387916
ShaneHenchin <shanehenchin@gmail.com>
2014-02-17 15:42:14 +0000

    Updates

--- js/save.js
+++ js/save.js
@@ -1,34 +1,78 @@
  $('#btnSave').click(function(){
- 	$.ajax({ 
-		url: 'http://henchinshoodies.comuv.com/save.php',
+
+    alert(book_details.title);
+
+    alert(book_details.authors);
+
+    alert(book_details.publisher);
+
+    var data = JSON.stringify(book_details);
+
+    $.ajax({ 
+
+        url: 'http://booksmart.webege.com/save.php',
+
          dataType: "json",
-         data: book_details,
+
+         data: {searchQuery: textVal},
+
          type: 'post',
+
          success: function(output) {
+
             alert("success");
+
          },
+
         error: function (request, status, error) {
+
             alert('error');
+
             alert(error);
+
         }
-	});
+
     });
 
-$('#searchBox').keyup(function(){
+    });
+
+ 
+
+ $('#searchBox').keyup(function(){
+
 var textVal = $("#searchBox").val();
+
 var data = {searchQuery: textVal};
+
+//alert(textVal);
+
 $.ajax({ url: 'http://henchinshoodies.comuv.com/search2.php',
+
          dataType: "json",
+
          data: {searchQuery: textVal},
+
          type: 'post',
+
          success: function(output) {
-            alert("success");
-            /*alert(output);*/
-                      $('#resultsDiv').html(output);
-        },
+
+            //alert(output);
+
+for(i=0; i<output.length; i++) {
+//alert(output[i].title);
+$('#resultsDiv').append(output[i].title+'<br>');
+}                      
+
+                  },
+
         error: function (request, status, error) {
+
             alert('error');
-           /* alert(error);*/
+
+            alert(error);
+
         }
+
 });
+
 });
